43b13e3aa7ab07d3a655909e541927ffaeaf924c
unknown <cksdyd8350@gmail.com>
2023-06-20 14:41:09 +0900

    Fixed router problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import kendoka from "./kendoka.svg";
 import "./App.css";
 import BuildingList from "./component/buildingList";
-import { Route, Routes } from "react-router-dom";
-import { NavigationLayout } from "./component/NavgationLayout"
+import { Route,Router, Routes } from "react-router-dom";
+import { NavigationLayout } from "./component/NavgationLayout";
 import { Home } from "./pages/Home";
 import { Buildings } from "./pages/Buildings";
 
 function App() {
   return (
     <div className="App">
-      <NavigationLayout>
-        <Routes>
-          <Route path="/bulding_list" element={<Buildings />} />
-        </Routes>
-      </NavigationLayout>
+        <NavigationLayout>
+          <Routes>
+            <Route path="/building_list" element={<Buildings />} />
+          </Routes>
+        </NavigationLayout>
     </div>
   );
 }
