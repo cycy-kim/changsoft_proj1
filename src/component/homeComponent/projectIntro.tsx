869ce4459ab37981a4f4b2b7a7e3280703796f0d
unknown <cksdyd8350@gmail.com>
2023-06-20 16:40:54 +0900

    now fetch data from server

--- src/component/homeComponent/projectIntro.tsx
+++ src/component/homeComponent/projectIntro.tsx
@@ -43,14 +43,31 @@ import {
 import axios from "axios";
 import { MultiSelectPropsContext } from "@progress/kendo-react-dropdowns";
 
-
-
 const ProjectIntro = () => {
+  const [projectNum, setProjectNum] = useState(0);
 
-  return <div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          //"http://192.168.0.129:8000/data/building",
+          "http://10.221.71.135:8000/data/building"
+        );
+        const data = JSON.parse(response.data);
+        //console.log(data);
+        setProjectNum(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-  </div>;
+  return (<div>
+    {projectNum}
+
+  </div>);
 };
 
 export default ProjectIntro;
