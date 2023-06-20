3bf767f8bdbddb68f2234d431c11ff98a6b81b8f
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-20 16:51:41 +0900

    modified Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,95 @@
 import React from 'react';
+import { TileLayout } from "@progress/kendo-react-layout";
+import { Button } from '@progress/kendo-react-buttons';
+
+const tiles = [
+  {
+    defaultPosition: {
+      col: 1,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 1",
+    body: <Button>Button 1</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 2,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 2",
+    body: <Button>Button 2</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 3,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 3",
+    body: <Button>Button 3</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 1,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 4",
+    body: <Button>Button 4</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 2,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 5",
+    body: <Button>Button 5</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 3,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 6",
+    body: <Button>Button 6</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 1,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 7",
+    body: <Button>Button 7</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 2,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 8",
+    body: <Button>Button 8</Button>,
+  },
+  {
+    defaultPosition: {
+      col: 3,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Tile 9",
+    body: <Button>Button 9</Button>,
+  },
+];
 
 export const Home: React.FC = () => {
   return (
     <div className="building-dashboard-container">
+      <TileLayout columns={3} items={tiles}></TileLayout>
     </div>
   );
 };
