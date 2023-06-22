3262c672c066746d2e6fed646a85683ad76b2f49
unknown <cksdyd8350@gmail.com>
2023-06-22 15:45:32 +0900

    implemented dropdownlist filtering in Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,25 +1,50 @@
 import BuildingList from "./../component/buildingList";
 import React, { useEffect, useState } from "react";
-import { DropDownList } from "@progress/kendo-react-dropdowns";
+import {
+  DropDownList,
+  DropDownListFilterChangeEvent,
+} from "@progress/kendo-react-dropdowns";
+import {
+  CompositeFilterDescriptor,
+  filterBy,
+  FilterDescriptor,
+} from "@progress/kendo-data-query";
 import axios from "axios";
 import urlPrefix from "./../resource/URL_prefix.json";
 
-interface projectList_interface{
-  project_name:string;
+interface projectList_interface {
+  project_name: string;
 }
 
 const Projects = () => {
-  const [projectList, setProjectList] = useState<projectList_interface[]>([]);
+  const [projectList, setProjectList] = useState<string[]>([]);
   const [selected, setSelected] = useState<string>("");
+  const [fileteredList,setFileteredList ] = useState<string[]>([]);
+
+  const filterData = (filter: FilterDescriptor | CompositeFilterDescriptor) => {
+    const data = projectList.slice();
+    return filterBy(data, filter);
+  };
+
+  const filterChange = (event: DropDownListFilterChangeEvent) => {
+    setFileteredList(filterData(event.filter));
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(urlPrefix.IP_port + "/project/project_name");
+        const response = await axios.get(
+          urlPrefix.IP_port + "/project/project_name"
+        );
         const data = JSON.parse(response.data);
 
-        const projectNames = data.map((obj:projectList_interface) => obj.project_name)
+        const projectNames = data.map(
+          (obj: projectList_interface) => obj.project_name
+        );
+
         setProjectList(projectNames);
+        setFileteredList(projectNames);
+
       } catch (error) {
         console.error(error);
       }
@@ -32,16 +57,13 @@ const Projects = () => {
     setSelected(event.target.value);
   };
 
-  const handleDropdownChange = (value: string) => {
-    setSelected(value);
-  };
-
   return (
     <div className="projects">
       <DropDownList
-        data={projectList}
+        data={fileteredList}
         value={selected}
         onChange={handleChange}
+        onFilterChange={filterChange}
         filterable={true}
         style={{ width: "300px" }}
       />
@@ -52,4 +74,4 @@ const Projects = () => {
   );
 };
 
-export {Projects}
+export default Projects;
