d3d10ff2883eb164b99f3b4a41cc47a97dc846bf
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-22 13:52:22 +0900

    modified home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,113 +1,63 @@
-import React from "react";
-import { TileLayout } from "@progress/kendo-react-layout";
+import React, { useState } from "react";
+import {
+  TileLayout,
+  TileLayoutRepositionEvent,
+} from "@progress/kendo-react-layout";
 import { Button } from "@progress/kendo-react-buttons";
 import TotalProject from "./../component/homeComponent/projectIntro";
-
 import ProjectDetail from "./../component/homeComponent/projectDetail";
 import DistributionMap from "./../component/homeComponent/distributionMap";
 import UsagePercentage from "./../component/homeComponent/usagePercentage";
 
 interface Tile {
-  defaultPosition: {
-    col: number;
-    colSpan: number;
-    rowSpan: number;
-  };
   header: string;
   body: React.ReactNode;
-  reorderable?: boolean;
-  draggable?: boolean;
 }
 
-const tiles= [
+const tiles: Tile[] = [
   {
-    defaultPosition: {
-      col: 1,
-      colSpan: 1,
-      rowSpan: 1,
-    },
     header: "Total Projects",
     body: <TotalProject />,
   },
   {
-    defaultPosition: {
-      col: 2,
-      colSpan: 1,
-      rowSpan: 1,
-    },
     header: "Total Buildings",
     body: <ProjectDetail />,
   },
   {
-    defaultPosition: {
-      col: 3,
-      colSpan: 1,
-      rowSpan: 1,
-    },
     header: "Distribution Map",
     body: <DistributionMap />,
-    reorderable: false,
   },
   {
-    defaultPosition: {
-      col: 1,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 4",
+    header: "Usage Percentage",
     body: <UsagePercentage />,
-  },
-  {
-    defaultPosition: {
-      col: 2,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 5",
-    body: <Button>Button 5</Button>,
-  },
-  {
-    defaultPosition: {
-      col: 3,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 6",
-    body: <Button>Button 6</Button>,
-  },
-  {
-    defaultPosition: {
-      col: 1,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 7",
-    body: <Button>Button 7</Button>,
-  },
-  {
-    defaultPosition: {
-      col: 2,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 8",
-    body: <Button>Button 8</Button>,
-  },
-  {
-    defaultPosition: {
-      col: 3,
-      colSpan: 1,
-      rowSpan: 1,
-    },
-    header: "Tile 9",
-    body: <Button>Button 9</Button>,
   },
 ];
 
 export const Home: React.FC = () => {
+  const [data, setData] = useState<
+    { col: number; colSpan: number; rowSpan: number }[]
+  >([
+    { col: 1, colSpan: 1, rowSpan: 1 },
+    { col: 1, colSpan: 1, rowSpan: 1 },
+    { col: 2, colSpan: 3, rowSpan: 1 },
+    { col: 2, colSpan: 3, rowSpan: 1 },
+  ]);
+
+  const handleReposition = (e: TileLayoutRepositionEvent) => {
+    setData(e.value);
+    console.log(e.value);
+  };
+
   return (
     <div className="building-dashboard-container">
-      <TileLayout columns={3} items={tiles} />
+      <TileLayout
+        columns={4}
+        rowHeight={255}
+        positions={data}
+        gap={{ rows: 10, columns: 10 }}
+        items={tiles}
+        onReposition={handleReposition}
+      />
     </div>
   );
 };
