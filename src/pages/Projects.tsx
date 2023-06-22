ae91093ff4346a6ca88054b0a6fee31df992d690
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-22 14:52:18 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    Committer: 김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
          modified:   src/pages/Projects.tsx
    
    modified

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
 import BuildingList from "./../component/buildingList";
+import React, { useEffect, useState } from "react";
+import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 export const Projects: React.FC = () => {
+  const [data, setData] = useState<string[]>([]);
+  const [value, setValue] = useState<string | null>(null);
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  useEffect(() => {
+    fetch("http://192.168.0.129:8000/project/project_name")
+      .then((response) => response.json())
+      .then((json) => {
+        const constructionCompanies = JSON.parse(json);
+        setData(constructionCompanies);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch construction companies:", error);
+      });
+  }, []);
+
+  const handleChange = (event: any) => {
+    setValue(event.target.value);
+  };
+
+  const handleDropdownChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
   return (
     <div className="projects">
-        <BuildingList />
+      <DropDownList
+        data={data}
+        value={value}
+        onChange={handleChange}
+        filterable={true}
+        style={{ width: "300px" }}
+      />
+      <div className="projects">
+        <BuildingList gridValue={selectedValue} />
+      </div>
     </div>
   );
 };
