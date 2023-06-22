aa7f4efb7775ad145c4df61cb15e39d60e69e300
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-22 16:08:47 +0900

    modified home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,16 +31,16 @@ const tiles: Tile[] = [
     body: <DistributionMap />,
   },
   {
+    header: "Local Percentage",
+    body: <LocalPercentage />,
+  },
+  {
     header: "Company Percentage",
     body: <CompanyPercentage />,
   },
   {
     header: "Usage Percentage",
     body: <UsagePercentage />,
-  },
-  {
-    header: "Local Percentage",
-    body: <LocalPercentage />,
   },
   {
     header: "Total Area",
@@ -52,13 +52,13 @@ export const Home: React.FC = () => {
   const [data, setData] = useState<
     { col: number; colSpan: number; rowSpan: number }[]
   >([
-    { col: 1, colSpan: 1, rowSpan: 1 },
-    { col: 1, colSpan: 1, rowSpan: 1 },
-    { col: 2, colSpan: 3, rowSpan: 2 },
-    { col: 2, colSpan: 1, rowSpan: 2 },
-    { col: 3, colSpan: 1, rowSpan: 2 },
-    { col: 4, colSpan: 1, rowSpan: 2 },
-    { col: 1, colSpan: 1, rowSpan: 2 },
+    { col: 1, colSpan: 1, rowSpan: 1 }, // Total project
+    { col: 1, colSpan: 1, rowSpan: 1 }, // Total Building
+    { col: 2, colSpan: 2, rowSpan: 2 }, // Map
+    { col: 4, colSpan: 1, rowSpan: 2 }, // Local %
+    { col: 1, colSpan: 1, rowSpan: 2 }, // 
+    { col: 2, colSpan: 1, rowSpan: 2 }, // 
+    { col: 3, colSpan: 2, rowSpan: 2 }, // 
   ]);
 
   const handleReposition = (e: TileLayoutRepositionEvent) => {
@@ -75,7 +75,7 @@ export const Home: React.FC = () => {
         gap={{ rows: 10, columns: 10 }}
         items={tiles.map((tile, index) => ({
           ...tile,
-          reorderable: index !== 2, // Set reorderable to false for Distribution Map tile
+          reorderable: tile.header !== "Distribution Map", // Set reorderable to false for Distribution Map tile
         }))}
         onReposition={handleReposition}
       />
