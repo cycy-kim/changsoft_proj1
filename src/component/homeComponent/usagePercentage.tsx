46729957a7c451778b7ac11e6c7075dd41700021
unknown <cksdyd8350@gmail.com>
2023-06-22 14:11:50 +0900

    modified percentage interface

--- src/component/homeComponent/usagePercentage.tsx
+++ src/component/homeComponent/usagePercentage.tsx
@@ -17,33 +17,33 @@ import {
 } from "@progress/kendo-react-data-tools";
 import "hammerjs";
 import axios from "axios";
-import { usagePercent_interface } from "../../interface/usagePercent_interface";
+import { percent_percentage } from "../../interface/percentage_interface";
 import urlPrefix from "./../../resource/URL_prefix.json";
 
 const labelContent = (e: any) =>
   e.category + "(" + (Number(e.percentage) * 100).toFixed(2) + "%)";
 
 const test_data_percentage = [
-  { sub_building_type: "type1", count: 150 },
-  { sub_building_type: "type2", count: 100 },
-  { sub_building_type: "type3", count: 80 },
-  { sub_building_type: "type4", count: 200 },
-  { sub_building_type: "type5", count: 300 },
+  { sub_building_type: "type1", count: 150 ,percentage : 0.1},
+  { sub_building_type: "type2", count: 100 ,percentage : 0.25},
+  { sub_building_type: "type3", count: 80 ,percentage : 0.15},
+  { sub_building_type: "type4", count: 200 ,percentage : 0.3},
+  { sub_building_type: "type5", count: 300 ,percentage : 0.2},
 ];
 
 const UsagePercentage = () => {
-  const [percentages, setPercentages] = useState<usagePercent_interface[]>([]);
+  const [percentages, setPercentages] = useState<percent_percentage[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         
         const response = await axios.get(urlPrefix.IP_port + "/dashboard/project");
-        const data1 = response.data;
-        setPercentages(data1);
+        const data = response.data;
+        setPercentages(data);
         
-        console.log(data1)
-        setPercentages(test_data_percentage);
+        console.log(data)
+        setPercentages(data);
       } catch (error) {
         console.error(error);
       }
--- src/interface/usagePercent_interface.ts
+++
@@ -1,4 +0,0 @@
-export interface usagePercent_interface {
-    sub_building_type :string;
-    count : number;
-  }
