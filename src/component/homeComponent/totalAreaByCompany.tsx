a5e0b8845310ded7ec6f3526963282ad3f8d7caf
71-hong-ii <kimkihong19@g.skku.edu>
2023-06-27 15:13:39 +0900

    modified Total Area by Company

--- src/component/homeComponent/totalAreaByCompany.tsx
+++ src/component/homeComponent/totalAreaByCompany.tsx
@@ -1,63 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { Tooltip } from '@progress/kendo-react-tooltip';
-import { TreeMap } from '@progress/kendo-react-treemap';
 import axios from 'axios';
+import { Chart, ChartSeries, ChartSeriesItem, ChartCategoryAxis, ChartCategoryAxisItem } from '@progress/kendo-react-charts';
 
-interface CompanyData {
-    construction_company: string;
-    total_area_sum: number;
-}
+export const TotalAreaByCompany = () => {
+  const [constructionData, setConstructionData] = useState<{ construction_company: string; total_area_sum: number }[]>([]);
 
-interface TreeItem {
-    name: string;
-    value: number;
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://10.221.72.80:8000/dashboard/project/construction_company_total_area');
+        const jsonData = JSON.parse(response.data);
+        setConstructionData(jsonData);
+      } catch (error) {
+        console.error('Error fetching construction data:', error);
+      }
+    };
 
-export const HeatmapView = () => {
-    const [data, setData] = useState<TreeItem[]>([]);
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get('http://10.221.72.80:8000/dashboard/project/construction_company_total_area');
-            const fetchedData: CompanyData[] = response.data;
-            const treeData: TreeItem[] = fetchedData.map(item => ({
-                name: item.construction_company,
-                value: item.total_area_sum,
-            }));
-            setData(treeData);
-        };
-        fetchData();
-    }, []);
-
-    const nFormatter = (num: number) => {
-        if (num >= 1000000000) {
-            return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
-        }
-        if (num >= 1000000) {
-            return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
-        }
-        if (num >= 1000) {
-            return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
-        }
-        return num;
-    }
-
-    const toolTipTemplate = (context: any) => {
-        const { text, dataItem } = context;
-        return (
-            <span>
-                <span>Company: {text}</span>
-                <br />
-                <span>Total Area: {nFormatter(dataItem.value)}</span>
-            </span>
-        );
-    }
-
-    return (
-        <div>
-            <Tooltip showCallout={false} content={toolTipTemplate}>
-                <TreeMap data={data} textField="name" valueField="value" colors={["#00AD51", "#00EF81"]} />
-            </Tooltip>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Chart style={{ height: "36vh" }}>
+        <ChartSeries>
+          <ChartSeriesItem
+            type="heatmap"
+            data={constructionData}
+            field="total_area_sum"
+            categoryField="construction_company"
+          />
+        </ChartSeries>
+        <ChartCategoryAxis>
+          <ChartCategoryAxisItem categories={constructionData.map(item => item.construction_company)} />
+        </ChartCategoryAxis>
+      </Chart>
+    </div>
+  );
+};
