493e41796bea691783053698d77e4d572140d743
71-hong-ii <kimkihong19@g.skku.edu>
2023-06-27 11:48:12 +0900

    modified charts

--- src/component/homeComponent/usagePercentage.tsx
+++ src/component/homeComponent/usagePercentage.tsx
@@ -1,41 +1,17 @@
-import React, {
-  useState,
-  useEffect,
-  SetStateAction,
-  JSXElementConstructor,
-} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Chart,
   ChartLegend,
   ChartTooltip,
   ChartSeries,
   ChartSeriesItem,
-  ChartLegendTitle,
-  ChartSeriesLabels,
 } from "@progress/kendo-react-charts";
-import {
-  setExpandedState,
-  setGroupIds,
-} from "@progress/kendo-react-data-tools";
-import "hammerjs";
 import axios from "axios";
-import { percent_percentage } from "../../interface/percentage_interface";
 import urlPrefix from "./../../resource/URL_prefix.json";
 import "./../../styles/ChartFont.scss";
 
-const labelContent = (e: any) =>
-  e.category + "(" + (Number(e.percentage) * 100).toFixed(2) + "%)";
-
-const test_data_percentage = [
-  { field: "type1", count: 150, percentage: 0.1 },
-  { field: "type2", count: 100, percentage: 0.25 },
-  { field: "type3", count: 80, percentage: 0.15 },
-  { field: "type4", count: 200, percentage: 0.3 },
-  { field: "type5", count: 300, percentage: 0.2 },
-];
-
 const UsagePercentage = () => {
-  const [percentages, setPercentages] = useState<percent_percentage[]>([]);
+  const [percentages, setPercentages] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,9 +20,20 @@ const UsagePercentage = () => {
           urlPrefix.IP_port + "/dashboard/project/usage_ratio"
         );
         const data = JSON.parse(response.data);
-        setPercentages(data);
 
-        //setPercentages(test_data_percentage);
+        // 상위 5개 데이터 추출
+        const top5 = data.slice(0, 5);
+
+        // 나머지 데이터 합산하여 "Others" 데이터 생성
+        const othersPercentage = data
+          .slice(5)
+          .reduce((acc: number, curr: any) => acc + curr.percentage, 0);
+        const othersData = { field: "Others", percentage: othersPercentage };
+
+        // "Others" 데이터를 포함하여 새로운 배열 생성
+        const modifiedData = [...top5, othersData];
+
+        setPercentages(modifiedData);
       } catch (error) {
         console.error(error);
       }
@@ -60,7 +47,7 @@ const UsagePercentage = () => {
       if (e.point.category === null) {
         return (
           <div>
-            <p>Category: NULL </p>
+            <p>Category: NULL</p>
             <p>Percentage: {Number(e.point.dataItem.percentage).toFixed(2)}%</p>
           </div>
         );
@@ -72,13 +59,13 @@ const UsagePercentage = () => {
         </div>
       );
     }
+    return null;
   };
 
   return (
     <div>
-      <Chart  style={{height: "36vh"}}>
-        <ChartLegend position="top">
-        </ChartLegend>
+      <Chart style={{ height: "36vh" }}>
+        <ChartLegend position="top" />
 
         <ChartSeries>
           <ChartSeriesItem
@@ -89,7 +76,7 @@ const UsagePercentage = () => {
             autoFit={true}
             holeSize={40}
             size={45}
-          ></ChartSeriesItem>
+          />
         </ChartSeries>
         <ChartTooltip render={renderTooltip} />
       </Chart>
