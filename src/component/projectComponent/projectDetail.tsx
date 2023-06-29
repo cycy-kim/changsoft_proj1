691278f1e02c3cb87b99b70e85f0350e947b69d1
71-hong-ii <kimkihong19@g.skku.edu>
2023-06-29 17:24:13 +0900

    styling project detail

--- src/component/projectComponent/projectDetail.tsx
+++ src/component/projectComponent/projectDetail.tsx
@@ -8,6 +8,7 @@ import {
 } from "@progress/kendo-react-grid";
 import axios from "axios";
 import urlPrefix from "../../resource/URL_prefix.json";
+import "../../styles/projectDetail.scss"
 
 interface projectDetail_interface {
   project_name: string;
@@ -20,6 +21,7 @@ interface projectDetail_interface {
   total_date: string;
   building_count: number;
 }
+
 const ProjectDetail = (props: any) => {
   const [projectData, setProjectData] = useState<projectDetail_interface[]>();
 
@@ -44,24 +46,65 @@ const ProjectDetail = (props: any) => {
     fetchData();
   }, [props]);
 
+  const headerClassName = "custom-header-cell";
+
   return (
     <div>
       <Grid data={projectData}>
-        <GridColumn field="project_name" title="프로젝트명" />
-        <GridColumn field="building_count" title="프로젝트 내 빌딩수" />
-      </Grid>
-      <Grid data={projectData}>
-        <GridColumn field="building_area" title="건축면적" />
-        <GridColumn field="construction_company" title="건설회사" />
-        <GridColumn field="location" title="지역" />
-        <GridColumn field="total_area" title="문서상 연면적" />
+        <GridColumn
+          field="project_name"
+          title="프로젝트명"
+          headerClassName={headerClassName}
+          className="custom-column"
+        />
+        <GridColumn
+          field="building_count"
+          title="프로젝트 내 빌딩수"
+          headerClassName={headerClassName}
+          className="custom-column"
+        />
       </Grid>
 
       <Grid data={projectData}>
-        <GridColumn title="프로젝트 기간">
-          <GridColumn field="construction_start" title="시작일" />
-          <GridColumn field="construction_end" title="종료일" />
-          <GridColumn field="total_date" title="소요일??" />
+        <GridColumn
+          field="building_area"
+          title="건축면적"
+          headerClassName={headerClassName}
+        />
+        <GridColumn
+          field="construction_company"
+          title="건설회사"
+          headerClassName={headerClassName}
+        />
+        <GridColumn
+          field="location"
+          title="지역"
+          headerClassName={headerClassName}
+        />
+        <GridColumn
+          field="total_area"
+          title="문서상 연면적"
+          headerClassName={headerClassName}
+        />
+      </Grid>
+
+      <Grid data={projectData}>
+        <GridColumn title="프로젝트 기간" headerClassName={headerClassName}>
+          <GridColumn
+            field="construction_start"
+            title="시작일"
+            headerClassName={headerClassName}
+          />
+          <GridColumn
+            field="construction_end"
+            title="종료일"
+            headerClassName={headerClassName}
+          />
+          <GridColumn
+            field="total_date"
+            title="소요일??"
+            headerClassName={headerClassName}
+          />
         </GridColumn>
       </Grid>
     </div>
