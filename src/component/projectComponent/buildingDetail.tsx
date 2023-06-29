f08e6ab11960c1faf8ae420b2b4ffeec9e9a4a57
71-hong-ii <kimkihong19@g.skku.edu>
2023-06-29 17:28:21 +0900

    modified

--- src/component/projectComponent/buildingDetail.tsx
+++ src/component/projectComponent/buildingDetail.tsx
@@ -12,6 +12,7 @@ import urlPrefix from "./../../resource/URL_prefix.json";
 import { useNavigate, Route, Routes } from "react-router-dom";
 import { buildingInfo_interface } from "./../../interface/buildingInfo_interface";
 import SubBuildingDetail from "./../SubBuildingComponent/subBuildingList";
+import "./../../styles/GridDetail.scss"
 
 const BuildingDetail = (props: any) => {
   const [imgPath, setImgPath] = useState<string>("");
@@ -55,6 +56,8 @@ const BuildingDetail = (props: any) => {
     navigate("/sub_building_detail");
   };
 
+  const headerClassName = "custom-header-cell";
+
   return (
     <div>
       <div style={{ width: "30%", float: "left", paddingLeft: "1%" }}>
@@ -66,23 +69,24 @@ const BuildingDetail = (props: any) => {
       </div>
       <div style={{ width: "59%", float: "right", paddingLeft: "1%" }}>
         <Grid data={[buildingInfo]}>
-          <GridColumn field="building_name" title="빌딩 이름" />
+          <GridColumn field="building_name" title="빌딩 이름" headerClassName={headerClassName} />
         </Grid>
         <Grid data={[buildingInfo]}>
-          <GridColumn field="total_area" title="total_area" />
-          <GridColumn field="stories" title="stories" />
-          <GridColumn field="height" title="height" />
-          <GridColumn field="construction_method" title="construction_method" />
+          <GridColumn field="total_area" title="total_area" headerClassName={headerClassName} />
+          <GridColumn field="stories" title="stories" headerClassName={headerClassName} />
+          <GridColumn field="height" title="height" headerClassName={headerClassName} />
+          <GridColumn field="construction_method" title="construction_method" headerClassName={headerClassName} />
         </Grid>
 
         <Grid data={[buildingInfo]}>
-          <GridColumn field="top_down" title="top_down" />
-          <GridColumn field="plane_shape" title="plane_shape" />
-          <GridColumn field="foundation_type" title="foundation_type" />
-          <GridColumn field="structure_code" title="structure_code" />
+          <GridColumn field="top_down" title="top_down" headerClassName={headerClassName} />
+          <GridColumn field="plane_shape" title="plane_shape" headerClassName={headerClassName} />
+          <GridColumn field="foundation_type" title="foundation_type" headerClassName={headerClassName} />
+          <GridColumn field="structure_code" title="structure_code" headerClassName={headerClassName} />
           <GridColumn
             field="performance_design_target"
             title="performance_design_target"
+            headerClassName={headerClassName}
           />
         </Grid>
       </div>
